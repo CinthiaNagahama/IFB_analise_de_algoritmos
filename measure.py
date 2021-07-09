a18943824dab11069635ee6dad625159ed44f2a6
Henrique Tavares Aguiar <ricktavares76@gmail.com>
2021-07-08 22:43:25 -0300

    removing large entries for bubble sort

--- measure.py
+++ measure.py
@@ -12,6 +12,8 @@ for sort_method in ("merge", "heap", "bubble"):
         csv_writer = csv.writer(csv_file, delimiter=";")
         csv_writer.writerow(["case", "method", "entries", "time(s)", "memory(kb)"])
 
-    for sort_case in ("random", "best", "worst"):
-        for entries in (1e4, 1e5, 1e6, 1e7, 1e8, 1e9) if sort_method != "bubble" else (1e4, 1e5, 1e6):
+    for sort_case in ("best", "random", "worst"):
+        for entries in (
+            (1e4, 1e5, 1e6) if sort_method == "bubble" and sort_case != "best" else (1e4, 1e5, 1e6, 1e7, 1e8, 1e9)
+        ):
             run(["./memusg.py", "./sort", sort_method, sort_case, "{:.0f}".format(entries)])
